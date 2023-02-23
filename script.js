823d067673d04b3f7c7355213162c1e5a9711569
Bekzat Bakirov <bekzat.bakirov.alt@gmail.com>
2023-02-24 00:21:49 +0600

    add animation

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-let a = document.querySelectorAll("a")
+// typed.js
+let typed = new Typed('#typed', { // Тут id того блока, в которм будет анимация
+    stringsElement: '#typed-strings', // Тут id блока из которого берем строки для анимации
+    typeSpeed: 100, // Скорость печати
+    startDelay: 500, // Задержка перед стартом анимации
+    backSpeed: 50, // Скорость удаления
+    loop: true // Указываем, повторять ли анимацию
+});
